efda31f7fdd2c392d2fc2ef95d9d4dd0974fb352
jayTseng <jay.tseng@aion.network>
2018-06-08 11:18:03 -0400

    update build script

--- script/postpack.sh
+++ script/postpack.sh
@@ -1,12 +1,20 @@
 # setup paths
 PACK_PATH="pack"
-BIN_NAME="modAionApi.jar"
+MOD_NAME="modAionApi.jar"
+LIB_NAME="libAionApi.jar"
+
 
 cd ${PACK_PATH}
 cp -r ../native .
-VER=$(java -jar ${BIN_NAME} -v)
+cp -r ../lib .
+cp ../docs/Java-API-*.doc.zip .
+rm lib/hamcrest-all-1.3.jar
+rm lib/junit-4.12.jar
+VER=$(java -jar ${MOD_NAME} -v)
 echo "Aion Api build ver - $VER"
-mv ${BIN_NAME} "modAionApi-v${VER}-$(date +%Y-%m-%d).jar"
+mv ${MOD_NAME} "modAionApi-v${VER}-$(date +%Y-%m-%d).jar"
 
+mv ${LIB_NAME} "libAionApi-v${VER}-$(date +%Y-%m-%d).jar"
+tar -czf "libAionApi-v${VER}-$(date +%Y-%m-%d).tar.gz" "libAionApi-v${VER}-$(date +%Y-%m-%d).jar" native lib Java-API-*.doc.zip
 cd ..
 git checkout -- ./src/org/aion/api/IAionAPI.java
